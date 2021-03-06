087f2fded69cee26783b2fdedf4afe68ef25b5c5
Amdiatou TIMERA <amdiatou.timera@ratp.fr>
2021-03-06 01:34:35 +0100

    Repartir de sero

---
+++ src/main/java/com/atimera/eshop/views/main/MainView.java
@@ -0,0 +1,13 @@
+package com.atimera.eshop.views.main;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends AppLayout {
+
+    public MainView() {
+
+    }
+
+}
